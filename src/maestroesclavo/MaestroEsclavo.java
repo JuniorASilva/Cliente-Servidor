2992bc969d550f2f3cc8944f6e136ac68829c286
Junior Silva <jsilvap22@gmail.com>
2017-10-17 11:32:35 -0500

    Iniciando Repo

---
+++ src/maestroesclavo/MaestroEsclavo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maestroesclavo;
+
+/**
+ *
+ * @author user
+ */
+public class MaestroEsclavo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
